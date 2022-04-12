726b31e01ca4e1738e8cfd82926439cef61216b6
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-04-12 17:16:51 +0300

    create Skills block

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Main.module.css';
+import styleContainer from '../common/styles/container.module.css'
 
 function Main() {
     return (
         <div className={style.mainBlock}>
-            <div className={style.container}>
+            <div className={styleContainer.container}>
                 <div className={style.intro}>
                     <span>Hi There</span>
                     <h1>I am DARIA GOLENKO</h1>
