f0f0cfcf852f0638be7158f6c0262a7308c94692
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-04-25 03:01:40 +0300

    add media@ for tablet and phone

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -5,12 +5,12 @@ import styleContainer from '../common/styles/container.module.css';
 function Main() {
     return (
         <div id={"main"} className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={style.container}>
                 <div className={style.photo}></div>
                 <div className={style.intro}>
                     <h1>I'm DARIA GOLENKO.</h1>
                     <span>Frontend React Developer</span>
-                    <div><a href={'https://drive.google.com/file/d/1WuMPMQNtiIwxkpYCT9syV1yMH434qxC-/view?usp=sharing'} className={style.buttonCV}>OPEN CV</a></div>
+                    <div><a target={"_blank"} href={'https://drive.google.com/file/d/1WuMPMQNtiIwxkpYCT9syV1yMH434qxC-/view?usp=sharing'} className={style.buttonCV}>OPEN CV</a></div>
                 </div>
             </div>
         </div>
